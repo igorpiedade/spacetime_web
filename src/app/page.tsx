efab60d0507ae0a4a057f3e1aa45d5933478699b
Igor Piedade <igorpiedade@hotmail.com>
2023-05-31 06:28:58 -0400

    callback route for github login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from 'next/headers'
+
 import SignIn from '@/components/SignIn'
 import Copyright from '@/components/Copyright'
 import Hero from '@/components/Hero'
 import EmptyMemory from '@/components/EmptyMemories'
+import Profile from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -13,7 +18,7 @@ export default function Home() {
         {/* Strips */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
 
         <Hero />
 
